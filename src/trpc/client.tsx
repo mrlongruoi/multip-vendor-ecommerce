af4e1531ee900df68333d99fc7e3e4de5bbe8b03
Mrlong101092 <mrlong01012020@gmail.com>
2025-04-18 23:51:28 +0700

    07: trpc integration fix

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -1,4 +1,6 @@
 'use client';
+
+import superjson from 'superjson'; // ^-- superjson is needed for serialization
 // ^-- to make sure we can mount the Provider from a server component
 import type { QueryClient } from '@tanstack/react-query';
 import { QueryClientProvider } from '@tanstack/react-query';
@@ -42,7 +44,7 @@ export function TRPCReactProvider(
     createTRPCClient<AppRouter>({
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: superjson,
           url: getUrl(),
         }),
       ],
