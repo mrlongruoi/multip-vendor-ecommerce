c7dfebe36920d78613db4bf55b2fd5e513743852
Mrlong101092 <mrlong01012020@gmail.com>
2025-04-18 17:39:39 +0700

    05: search filters

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,18 +1,12 @@
-import configPromise from '@payload-config'
-import { getPayload } from 'payload'
 
-export default async function Home() {
 
-    const payload = await getPayload({
-      config: configPromise,
-    })
+export default function Home() {
 
-    const data = await payload.find({
-      collection: 'categories',
-    })
+  
+    
   return (
     <div>
-      {JSON.stringify(data,null,2)}
+     Home Page
     </div>
   );
 }
