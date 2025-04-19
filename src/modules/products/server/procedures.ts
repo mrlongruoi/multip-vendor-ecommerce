f70b59894ebcae653d2eb63e0787837f4742931f
Mrlong101092 <mrlong01012020@gmail.com>
2025-04-20 01:42:06 +0700

    12: filtering products fix 2

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -6,26 +6,29 @@ export const productsRouter = createTRPCRouter({
   getMany: baseProcedure
     .input(
       z.object({
-        category: z.string().nullable().optional(),        
-        minPrice: z.string().nullable().optional(),        
-        maxPrice: z.string().nullable().optional(),        
+        category: z.string().nullable().optional(),
+        minPrice: z.string().nullable().optional(),
+        maxPrice: z.string().nullable().optional(),
       })
     )
     .query(async ({ ctx, input }) => {
       const where: Where = {};
 
-      if (input.minPrice) {
+      if (input.minPrice && input.maxPrice) {
         where.price = {
           ...where.price,
-          greater_than_equal: input.minPrice
-        }
-      }
-
-      if (input.maxPrice) {
+          less_than_equal: input.maxPrice,
+          greater_than_equal: input.minPrice,
+        };
+      } else if (input.minPrice) {
+        where.price = {
+          greater_than_equal: input.minPrice,
+        };
+      } else if (input.maxPrice) {
         where.price = {
           ...where.price,
-          less_than_equal: input.maxPrice
-        }
+          less_than_equal: input.maxPrice,
+        };
       }
 
       if (input.category) {
@@ -40,7 +43,6 @@ export const productsRouter = createTRPCRouter({
             },
           },
         });
-        
 
         const formattedData = categoriesData.docs.map((doc) => ({
           ...doc,
@@ -49,13 +51,17 @@ export const productsRouter = createTRPCRouter({
             subcategories: undefined,
           })),
         }));
-        const subcategoriesSlugs =[];
+        const subcategoriesSlugs = [];
         const parentCategory = formattedData[0];
         if (parentCategory) {
-          subcategoriesSlugs.push(...parentCategory.subcategories.map((subcategory)=>subcategory.slug))
+          subcategoriesSlugs.push(
+            ...parentCategory.subcategories.map(
+              (subcategory) => subcategory.slug
+            )
+          );
           where["category.slug"] = {
             in: [parentCategory.slug, ...subcategoriesSlugs],
-          };       
+          };
         }
       }
       const data = await ctx.db.find({
