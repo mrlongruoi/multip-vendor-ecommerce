ae8f2ff8b9278eb636d6a17b6e66268fc95042d7
Mrlong101092 <mrlong01012020@gmail.com>
2025-04-20 05:53:46 +0700

    13: api filters sort

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,7 +1,8 @@
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { z } from "zod";
-import type { Where } from "payload";
+import type { Sort, Where } from "payload";
 import { Category } from "@/payload-types";
+import { sortValues } from "../search-params";
 export const productsRouter = createTRPCRouter({
   getMany: baseProcedure
     .input(
@@ -9,16 +10,31 @@ export const productsRouter = createTRPCRouter({
         category: z.string().nullable().optional(),
         minPrice: z.string().nullable().optional(),
         maxPrice: z.string().nullable().optional(),
+        tags: z.array(z.string()).nullable().optional(),
+        sort: z.enum(sortValues).nullable().optional(),
       })
     )
     .query(async ({ ctx, input }) => {
       const where: Where = {};
 
+      let sort: Sort = "-createdAt";
+
+      if (input.sort === "curated") {
+        sort = "-createdAt";
+      }
+
+      if (input.sort === "hot_and_new") {
+        sort = "+createdAt";
+      }
+      if (input.sort === "trending") {
+        sort = "-createdAt";
+      }
+
       if (input.minPrice && input.maxPrice) {
         where.price = {
           ...where.price,
-          less_than_equal: input.maxPrice,
           greater_than_equal: input.minPrice,
+          less_than_equal: input.maxPrice,
         };
       } else if (input.minPrice) {
         where.price = {
@@ -64,10 +80,18 @@ export const productsRouter = createTRPCRouter({
           };
         }
       }
+
+      if (input.tags && input.tags.length > 0) {
+        where["tags.name"] = {
+          in: input.tags,
+        };
+      }
+
       const data = await ctx.db.find({
         collection: "products",
         depth: 1,
         where,
+        sort,
       });
       return data;
     }),
