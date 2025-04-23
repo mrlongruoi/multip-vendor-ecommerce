db1bb7f6aee472d5782cd76f823174d5e0d7be4e
Mrlong101092 <mrlong01012020@gmail.com>
2025-04-23 22:31:17 +0700

    17: product page

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -5,6 +5,26 @@ import { Category, Media, Tenant } from "@/payload-types";
 import { sortValues } from "../search-params";
 import { DEFAULT_LIMIT } from "@/constants";
 export const productsRouter = createTRPCRouter({
+
+  getOne: baseProcedure
+  .input(
+    z.object({
+      id: z.string(),
+    })
+  )
+  .query(async ({ ctx, input }) => {
+    const product = await ctx.db.findByID({
+      collection: "products",
+      id: input.id,
+      depth : 2,
+    });
+    return {
+      ...product,
+      image: product.image as Media | null,
+      tenant: product.tenant as Tenant & { image: Media | null },
+    }
+  }),
+  
   getMany: baseProcedure
     .input(
       z.object({
--- src/modules/products/ui/components/product-card.tsx
+++ src/modules/products/ui/components/product-card.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { StarIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { generateTenantURL } from "@/lib/utils";
+import { formatCurrency, generateTenantURL } from "@/lib/utils";
 
 interface ProductCardProps {
   id: string;
@@ -37,7 +37,7 @@ const handleUserClick = (e: React.MouseEvent<HTMLDivElement>) => {
 };
 
   return (
-    <Link href={`/products/${id}`}>
+    <Link href={`${generateTenantURL(tenantSlug)}/products/${id}`}>
       <div className="hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-shadow border rounded-md bg-white overflow-hidden h-full flex flex-col">
         <div className="relative aspect-square">
           <Image
@@ -73,11 +73,7 @@ const handleUserClick = (e: React.MouseEvent<HTMLDivElement>) => {
         <div className="p-4">
           <div className="relative px-2 py-1 border bg-pink-400 w-fit">
             <p className="text-sm font-medium">
-              {new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
-                maximumFractionDigits: 0,
-              }).format(Number(price))}
+              {formatCurrency(price)}
             </p>
           </div>
         </div>
