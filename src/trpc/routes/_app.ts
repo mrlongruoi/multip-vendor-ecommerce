7dffb0082e49c3b16da908897f91c8bf0f48dd53
Mrlong101092 <mrlong01012020@gmail.com>
2025-04-24 03:00:51 +0700

    19: checkout page

--- src/trpc/routes/_app.ts
+++ src/trpc/routes/_app.ts
@@ -1,14 +1,16 @@
+import { createTRPCRouter } from '../init';
 
 import { authRouter } from '@/modules/auth/server/procedures';
-import { createTRPCRouter } from '../init';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
 import { productsRouter } from '@/modules/products/server/procedures';
 import { tagsRouter } from '@/modules/tags/server/procedures';
 import { tenantsRouter } from '@/modules/tenants/server/procedures';
+import { checkoutRouter } from '@/modules/checkout/server/procedures';
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   tags: tagsRouter,
   tenants: tenantsRouter,
+  checkout: checkoutRouter,
   products: productsRouter,
   categories: categoriesRouter
 });
