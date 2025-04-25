a0de323b9366a0afee5ec5a029c3e6e115a0a4b0
Mrlong101092 <mrlong01012020@gmail.com>
2025-04-26 00:36:41 +0700

    27: subdomain rewrites

--- src/modules/checkout/server/procedures.ts
+++ src/modules/checkout/server/procedures.ts
@@ -10,6 +10,7 @@ import Stripe from "stripe";
 import { CheckoutMetadata, ProductMetadata } from "../types";
 import { stripe } from "@/lib/stripe";
 import { PLATFORM_FEE_PERCENTAGE } from "@/constants";
+import { generateTenantURL } from "@/lib/utils";
 
 export const checkoutRouter = createTRPCRouter({
 
@@ -155,11 +156,19 @@ export const checkoutRouter = createTRPCRouter({
           totalAmount * (PLATFORM_FEE_PERCENTAGE / 100)
         );
 
+        const domain = generateTenantURL(input.tenantSlug);
+
+        // if (process.env.NODE_ENV === "development") {
+        //   domain = `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${input.tenantSlug}`;
+        // } else {
+        //   domain = `${input.tenantSlug}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`;
+        // }
+
 
         const checkout = await stripe.checkout.sessions.create({
           customer_email: ctx.session.user.email,
-          success_url: `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${input.tenantSlug}/checkout?success=true`,
-          cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${input.tenantSlug}/checkout?cancel=true`,
+          success_url: `${domain}/checkout?success=true`,
+          cancel_url: `${domain}/checkout?cancel=true`,
           mode: "payment",
           line_items: lineItems,
           invoice_creation:{
